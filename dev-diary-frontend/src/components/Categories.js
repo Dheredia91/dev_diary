4d14b01bc81d3c79a8fcced5e9c95bd9bc271e84
dheredia91 <113152681+Dheredia91@users.noreply.github.com>
2024-07-19 23:42:13 -0700

    Create notes/categories react components

---
+++ dev-diary-frontend/src/components/Categories.js
@@ -0,0 +1,32 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchCategories } from '../slices/categorySlice';
+
+/* Categories react component, for more info see Notes.js*/
+
+const Categories = () => {
+  const dispatch = useDispatch();
+  const categories = useSelector((state) => state.categories.categories);
+  const loading = useSelector((state) => state.categories.loading);
+  const error = useSelector((state) => state.categories.error);
+
+  useEffect(() => {
+    dispatch(fetchCategories());
+  }, [dispatch]);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error}</p>;
+
+  return (
+    <div>
+      <h1>Categories</h1>
+      <ul>
+        {categories.map((category) => (
+          <li key={category.id}>{category.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Categories;
---
+++ dev-diary-frontend/src/components/Notes.js
@@ -0,0 +1,40 @@
+// src/components/Notes.js
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchNotes } from '../slices/noteSlice';
+
+const Notes = () => {
+  const dispatch = useDispatch();
+  const notes = useSelector((state) => state.notes.notes);
+  const loading = useSelector((state) => state.notes.loading);
+  const error = useSelector((state) => state.notes.error);
+
+  // need to use useEffect hook to synchornize component with external system (Redux)
+  // this calls dispatch to start the process of communicating with the API (see fetchNotes in noteSlice)
+  useEffect(() => {
+    dispatch(fetchNotes());
+  }, [dispatch]);
+
+  if (loading){
+    return <p>Loading...</p>;
+  } 
+  if (error){
+    return <p>Error: {error}</p>;
+  } 
+
+  // using jsx here to map notes to a list
+  return (
+    <div>
+      <h1>Notes</h1>
+      <ul>
+        {notes.map((note) => (
+          <li key={note.id}>
+            {note.title}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Notes;
