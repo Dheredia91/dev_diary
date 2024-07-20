4ca84504805c3cd1c98a4573cf715a8c25eff352
dheredia91 <113152681+Dheredia91@users.noreply.github.com>
2024-07-19 22:20:51 -0700

    modify index.js to use Provider to wrap app with Redux store

--- dev-diary-frontend/src/index.js
+++ dev-diary-frontend/src/index.js
@@ -1,17 +1,22 @@
+// src/index.js
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client'; 
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import store from './store/store';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// create the root element and use createRoot (React 18)
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+// Provider component wraps the entire app
+// making the Redux store available to all components
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
